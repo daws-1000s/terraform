18467d1e0301a50fb75a96a160fd1be56a014fae
daws-100s <narayanalaxmanmangena@gmail.com>
2025-11-10 13:18:06 +0530

    for-each

---
+++ dynamic/output.tf
@@ -0,0 +1,3 @@
+output "vpc_id" {
+    value = aws_security_group.expense.id
+}
---
+++ dynamic/sg.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "expense" {
+    name = "allow-ssh-loops-dynamic"
+    description = "allow ssh port number 22"
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "tcp"
+        cidr_blocks = [ "0.0.0.0/0" ]
+        ipv6_cidr_blocks = [ "::/0" ]
+    }
+
+    dynamic "ingress" {
+        for_each = var.ingress_rules
+        content {
+          from_port = ingress.value["from_port"]
+          to_port = ingress.value["to_port"]
+          protocol = ingress.value["protocol"]
+          cidr_blocks = ingress.value.cidr_blocks
+        }
+    }
+
+    tags = {
+      Name = "terraform"
+    }
+}
